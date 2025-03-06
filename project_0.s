ca99a4a828254b59b140fac445dc1dc3afef5c7c
dimi11433 <164883456+dimi11433@users.noreply.github.com>
2025-03-06 14:02:33 -0500

    Update project_0.s
    In this second commit all we did was begin the main section in doing so we initialized the hard-coded N value specific to my Howard ID. We then Initialized the m-counter.

--- project_0.s
+++ project_0.s
@@ -3,3 +3,10 @@ id: .asciiz "@03100118" My unique Howard ID
 
 .text
 .globl main
+
+main:
+#Let's Initialize the hard-coded N value 
+    li $s0, 6 #$s0 = N = 03100118 % 8 = 6
+
+    #Print the forward strings
+    li $t0, 1 #Lets Initialize the m-counter (1-9)
